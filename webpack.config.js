8e8f2658cad1cd84dc51e3b81a2628773ba7f847
1740249919@qq.com <1740249919@qq.com>
2019-04-25 21:29:29 +0800

    html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,15 @@
 const path = require("path");
+const webpack = require("webpack");
+const resolvePath = dir => path.resolve(__dirname, dir);
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  mode: "development"
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolvePath("public/index.html"),
+      filename: "index.html",
+      inject: "body",
+      title: "Webpack App"
+    })
+  ]
 };
