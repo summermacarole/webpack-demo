b1895af8245d15d292276dca38ffffff082ec9c1
1740249919@qq.com <1740249919@qq.com>
2019-04-25 22:28:23 +0800

    打包报错提示

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 const resolvePath = dir => path.resolve(__dirname, dir);
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const SystemBellPlugin = require("system-bell-webpack-plugin");
 module.exports = {
   mode: "development",
   devServer: {
@@ -17,8 +18,9 @@ module.exports = {
     }
   },
   plugins: [
+    new SystemBellPlugin(),
     new HtmlWebpackPlugin({
-      template: resolvePath("public/index.html"),
+      template: resolvePath("pulic/index.html"),
       filename: "index.html",
       inject: "body",
       title: "Webpack App"
