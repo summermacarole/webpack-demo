75ea9dbcf741b65dcf328af42bb7caaecf0ad6b4
1740249919@qq.com <1740249919@qq.com>
2019-04-25 23:09:26 +0800

    配置对象组合

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,37 @@
 const path = require("path");
 const webpack = require("webpack");
-const resolvePath = dir => path.resolve(__dirname, dir);
+const merge = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const SystemBellPlugin = require("system-bell-webpack-plugin");
-module.exports = {
-  mode: "development",
-  devServer: {
-    contentBase: "dist",
-    stats: "errors-only",
-    host: "0.0.0.0",
-    historyApiFallback: true,
-    overlay: true,
-    watchOptions: {
-      ignored: /node_modules/,
-      aggregateTimeout: 300,
-      poll: 1000
-    }
-  },
-  plugins: [
-    new SystemBellPlugin(),
-    new HtmlWebpackPlugin({
-      template: resolvePath("pulic/index.html"),
-      filename: "index.html",
-      inject: "body",
-      title: "Webpack App"
-    })
-  ]
+const resolvePath = dir => path.resolve(__dirname, dir);
+
+const parts = require("./webpack.parts");
+
+const commonConfig = merge([
+  {
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: resolvePath("public/index.html"),
+        filename: "index.html",
+        inject: "body",
+        title: "Webpack App"
+      })
+    ]
+  }
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    host: process.env.HOST,
+    port: process.env.PORT
+  })
+]);
+
+module.exports = mode => {
+  console.log(mode);
+  if (mode == "production") {
+    return merge(commonConfig, productionConfig, { mode });
+  }
+  return merge([commonConfig, developmentConfig, { mode }]);
 };
